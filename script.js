60ea727bb498ccb144fc376452abaf6a24917a08
Dax-F <daxf0824@scsd2.com>
2026-02-05 16:12:09 +0000

    Test

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 var textt = prompt("This is a Javascript test: ");
-var par = document.createElement("p");
-var node = document.createTextNode("Javascript response test: " + textt);
-par.appendChild(node);
-var elem = document.getElementById("div1");
-elem.appendChild(par);
+console.log(textt);
