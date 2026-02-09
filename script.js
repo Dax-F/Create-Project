ac85cc53a3324d0820bbf0645f246ed0010f816e
Dax-F <daxf0824@scsd2.com>
2026-02-09 15:52:58 +0000

    Adding Images

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 var textbox = document.getElementById("textbox");
 
 var act = 2;
+
+var enterTree = ["(The Favor Tree.)"
+    ,"(Isabeau said the tree felt &#34weird&#34...)"
+    ,"(And you heard someone telling you to come here, didn't you?)"
+    ,"(What was that voice...?)"
+    ,"(...)"
+    ,"(You walk towards the tree.)"
+    ,"(...The tree is so big, everything under it is dark as night, but...)"
+    ,"(You feel like someone is here, sitting at the foot of the tree.)"
+    ,"(Looking at you.)"
+    ,"(Cautiously, with a hand on your dagger, you walk towards the base of the tree.)"
+    ,"(. . .)"
+    ,"(Well, uh, you found someone alright.)"
+    ,"&#34Uh, hello, stranger.&#34"
+    ,"...Hello, stranger."]
+var meetLoop = [
+    {text:"", image:""}
+]
